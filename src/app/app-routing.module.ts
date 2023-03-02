7a598f3178aa6afc8f5c369800d98447d71b8ecf
och-karr <okaa93@gmail.com>
2023-03-02 14:02:56 +0100

    feat: add logged-in component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {UserServiceModule} from './services/user.service-module';
+import {LoginComponentModule} from './components/login/login.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([
+    {
+      path: 'login',
+      component: LoginComponent
+    }
+  ]), UserServiceModule, LoginComponentModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
