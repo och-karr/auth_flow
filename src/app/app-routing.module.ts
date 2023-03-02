bccc1013afbe6885300c38819304e00ce4705202
och-karr <okaa93@gmail.com>
2023-03-02 12:18:07 +0100

    feat: add route to register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponentModule } from './components/register/register.component-module';
+import { UserServiceModule } from './services/user.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{ path: 'register', component: RegisterComponent }]), RegisterComponentModule, UserServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
