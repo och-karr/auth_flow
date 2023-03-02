cef6169a09b948579ad7e47e53d5d2cf279c7145
och-karr <okaa93@gmail.com>
2023-03-02 12:25:35 +0100

    feat: add login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 import { RegisterComponentModule } from './components/register/register.component-module';
 import { UserServiceModule } from './services/user.service-module';
+import { LoginComponentModule } from './components/login/login.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'register', component: RegisterComponent }]), RegisterComponentModule, UserServiceModule],
+  imports: [RouterModule.forRoot([{ path: 'register', component: RegisterComponent }, { path: 'login', component: LoginComponent }]), RegisterComponentModule, UserServiceModule, LoginComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
