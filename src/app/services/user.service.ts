86a920d83c6adb7d7546bd40df695ef06b53a420
och-karr <okaa93@gmail.com>
2023-03-06 11:07:57 +0100

    feat: save accessToken to local storage

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from "rxjs";
+import {Observable, tap} from "rxjs";
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
@@ -9,6 +9,10 @@ export class UserService {
   }
 
   login(data: any): Observable<any> {
-    return this._httpClient.post<any>('https://us-central1-courses-auth.cloudfunctions.net/auth/login', data);
+    return this._httpClient.post<any>('https://us-central1-courses-auth.cloudfunctions.net/auth/login', data).pipe(
+      tap(val => {
+        localStorage.setItem('accessToken', val.data.accessToken);
+      })
+    );
   }
 }
