90424e7be00716f9264adf41ac2350dce6a8fd59
och-karr <okaa93@gmail.com>
2023-03-06 10:31:37 +0100

    feat: add verify guard to router and app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,38 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { LoggedInComponent } from './components/logged-in/logged-in.component';
-import { VerifyComponent } from './components/verify/verify.component';
-import { RegisterComponentModule } from './components/register/register.component-module';
-import { UserServiceModule } from './services/user.service-module';
-import { LoginComponentModule } from './components/login/login.component-module';
-import { LoggedInComponentModule } from './components/logged-in/logged-in.component-module';
-import { VerifyComponentModule } from './components/verify/verify.component-module';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {RegisterComponent} from './components/register/register.component';
+import {LoginComponent} from './components/login/login.component';
+import {LoggedInComponent} from './components/logged-in/logged-in.component';
+import {VerifyComponent} from './components/verify/verify.component';
+import {RegisterComponentModule} from './components/register/register.component-module';
+import {UserServiceModule} from './services/user.service-module';
+import {LoginComponentModule} from './components/login/login.component-module';
+import {LoggedInComponentModule} from './components/logged-in/logged-in.component-module';
+import {VerifyComponentModule} from './components/verify/verify.component-module';
+import {VerifyGuard} from "./guards/verify/verify.guard";
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'register', component: RegisterComponent }, { path: 'login', component: LoginComponent }, { path: 'logged-in', component: LoggedInComponent }, { path: 'verify', component: VerifyComponent }]), RegisterComponentModule, UserServiceModule, LoginComponentModule, LoggedInComponentModule, VerifyComponentModule],
+  imports: [RouterModule.forRoot([
+    {
+      path: 'register',
+      component: RegisterComponent
+    },
+    {
+      path: 'login',
+      component: LoginComponent
+    },
+    {
+      path: 'logged-in',
+      component: LoggedInComponent,
+      canActivate: [VerifyGuard],
+      data: {redirectUrl: '/verify'}
+    },
+    {
+      path: 'verify',
+      component: VerifyComponent
+    }
+  ]), RegisterComponentModule, UserServiceModule, LoginComponentModule, LoggedInComponentModule, VerifyComponentModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from "@angular/common/http";
+import {VerifyGuard} from "./guards/verify/verify.guard";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { HttpClientModule } from "@angular/common/http";
     BrowserAnimationsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    VerifyGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
