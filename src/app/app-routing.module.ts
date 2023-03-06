89e217d7c6c81796b0cd132fa63eea842c9017e4
och-karr <okaa93@gmail.com>
2023-03-06 13:06:03 +0100

    feat: add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {LoginAdminComponent} from './components/login-admin/login-admin.component';
-import {LoginUserComponent} from './components/login-user/login-user.component';
-import {LoginAdminComponentModule} from './components/login-admin/login-admin.component-module';
-import {UserServiceModule} from './services/user.service-module';
-import {LoginUserComponentModule} from './components/login-user/login-user.component-module';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { LoginAdminComponent } from './components/login-admin/login-admin.component';
+import { LoginUserComponent } from './components/login-user/login-user.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginAdminComponentModule } from './components/login-admin/login-admin.component-module';
+import { UserServiceModule } from './services/user.service-module';
+import { LoginUserComponentModule } from './components/login-user/login-user.component-module';
+import { HomeComponentModule } from './components/home/home.component-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([
@@ -15,8 +17,12 @@ import {LoginUserComponentModule} from './components/login-user/login-user.compo
     {
       path: 'login-user',
       component: LoginUserComponent
+    },
+    {
+      path: '',
+      component: HomeComponent
     }
-  ]), LoginAdminComponentModule, UserServiceModule, LoginUserComponentModule],
+  ]), LoginAdminComponentModule, UserServiceModule, LoginUserComponentModule, HomeComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  styleUrls: ['./home.component.scss'],
+  templateUrl: './home.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent {
+
+  logoutUser() {
+    localStorage.removeItem('accessToken');
+    window.location.reload();
+  }
+}
