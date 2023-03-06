560a4bdfe097b7f8e3fb72438b5481fbe42bfa81
och-karr <okaa93@gmail.com>
2023-03-06 13:06:03 +0100

    feat: add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,20 @@ import { RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LoggedInComponent } from './components/logged-in/logged-in.component';
 import { CompleteProfileComponent } from './components/complete-profile/complete-profile.component';
+import { HomeComponent } from './components/home/home.component';
 import { UserServiceModule } from './services/user.service-module';
 import { LoginComponentModule } from './components/login/login.component-module';
 import { LoggedInComponentModule } from './components/logged-in/logged-in.component-module';
 import { CompleteProfileComponentModule } from './components/complete-profile/complete-profile.component-module';
+import { HomeComponentModule } from './components/home/home.component-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([
     { path: 'login', component: LoginComponent },
     { path: 'logged-in', component: LoggedInComponent },
-    { path: 'complete-profile', component: CompleteProfileComponent }
-  ]), UserServiceModule, LoginComponentModule, LoggedInComponentModule, CompleteProfileComponentModule],
+    { path: 'complete-profile', component: CompleteProfileComponent },
+    { path: '', component: HomeComponent }
+  ]), UserServiceModule, LoginComponentModule, LoggedInComponentModule, CompleteProfileComponentModule, HomeComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
